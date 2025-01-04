0787b4dc5b5542abf97e49156ab92c8737b2916a
Varad J <varad.gcs@gmail.com>
2025-01-04 15:29:33 +0530

    Done building memory game

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased flex flex-col w-screen items-center p-4">
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
+
+      <body className="antialiased flex flex-col h-dvh w-screen items-center justify-center p-4 bg-gray-50 text-3xl text-gray-700 font-black dark:text-gray-300 dark:bg-gray-900">
         <Main />
         <NextScript />
       </body>
--- src/pages/memory-game.js
+++ src/pages/memory-game.js
@@ -4,32 +4,111 @@ export default function MemoryGame() {
     const [tiles, setTiles] = useState([]);
     const [isGameStarted, setisGameStarted] = useState(false);
     const [timer, setTimer] = useState(0);
+    const [flippedTiles, setflippedTiles] = useState([])
+    const [moves, setMoves] = useState(0)
+    const [score, setScore] = useState(0)
     const startGame = () => {
         initializeTiles();
         setisGameStarted(true);
+        setTimer(0);
+        setMoves(0);
+        setScore(0);
     }
+
     const initializeTiles = () => {
-        const tilesArray = ['A', 'B', 'C', 'D', 'A', 'B', 'C', 'D'];
+        const tilesArray = ['home', 'delete', 'favorite', 'search', 'bolt', 'home', 'delete', 'favorite', 'search', 'bolt'];
         setTiles(tilesArray.sort(() => Math.random() -0.5 ));
-        
     }
+
+    const flipTile = (tileIndex) => {
+        if (flippedTiles.length === 2) {
+            
+            setflippedTiles([]);
+        }
+        setMoves(prevMoves => prevMoves+1)
+        if (flippedTiles.includes(tileIndex)) return;
+        setflippedTiles((prevFlippedTiles) => [...prevFlippedTiles, tileIndex] )
+            const [firstTileIndex] = flippedTiles;
+            const firstTile = tiles[firstTileIndex];
+            const secondTile = tiles[tileIndex];
+            console.log(firstTile, secondTile)
+            if (firstTile != null && secondTile != null) {
+                if (firstTile != secondTile) {
+                    console.log("wrong")
+                } else {
+                    setScore((prevScore) => prevScore+1)
+                    setTimeout(() => {
+                        setTiles((prevTiles) => {
+                            const newTiles = [...prevTiles];
+                            newTiles[firstTileIndex] = null; // Remove first matched tile
+                            newTiles[tileIndex] = null; // Remove second matched tile
+                            return newTiles;
+                        });
+                        setScore((prevScore) => prevScore + 1); // Increase score for correct match
+                    }, 1000);                 
+                }   
+            }
+    }
+
+    useEffect(() => {
+        let intervalId;
+        if (isGameStarted) {
+            intervalId = setInterval(() => {
+                setTimer(prevTimer => prevTimer + 1);
+                console.log(timer)
+            }, 1000);
+        }
+        
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [isGameStarted]);
+    
+    useEffect(() => {
+        if(isGameStarted) {
+        if(tiles.every(tile => tile ===null)) {
+            alert(`You won! Time: ${timer}, Moves: ${moves}, Score: ${score}`)
+        }
+        }
+    }, [tiles, isGameStarted])
+
     return(
         <div>
             {!isGameStarted ? (
             <button 
-                className='bg-gray-300 p-2 rounded-md text-gray-600'
+                className='bg-yellow-200 p-20 aspect-square rounded-md text-rose-900 font-mono border border-yellow-400 hover:bg-yellow-100 transition-all duration-300 ease-in-out'
                 onClick={startGame}
-            >Start Game</button>
+            >Game Start!</button>
             ) : (
-                <div>
-                <h1>Game Started</h1>
-                <div>
-                {tiles.map((tile, index) => (
-                    <button>
-                        {tile}
-                    </button>
-                ))}
-                </div>
+                <div 
+                    className="flex flex-col items-center gap-6"
+                >
+                    <h1>Game Started</h1>
+                    <div 
+                        className='grid grid-cols-5 w-full gap-2'
+                    >
+                        {tiles.map((tile, index) => (
+                            <button 
+                                key={index}
+                                className="bg-yellow-200 text-rose-600 aspect-square p-20 rounded-3xl text-3xl border border-rose-600 ring-0 focus:ring-2 focus:ring-rose-600 hover:bg-gray-300 transition-all duration-300 ease-in-out"
+                                onClick={() => {flipTile(index)}}
+                            >
+                                {
+                                flippedTiles.includes(index) ?
+                                <span className='material-icons'>  
+                                    {tile} 
+                                </span>
+                                : 
+                                <span className='material-icons text-rose-900'>  
+                                    X
+                                </span>
+                                }
+                            </button>
+                        ))}
+                    </div>
+                    <h1>Time elapsed: {timer} secs</h1>
+                    <h1>Moves: {moves}</h1>
+                    <h1>Score: {score}</h1>
                 </div>
             )}
         </div>
